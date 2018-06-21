b8432a6c67ea7a67d5c4aff164c70399df90ed22
jiraiyasennin <d.ugel@yahoo.com>
2018-06-21 08:54:18 +0200

    Primer commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<!--
+**Desarrollo de un Sistema gestor de contenidos con PHP, SQL Y BOOTSTRAP**
+                        By Dostow Ugel
+-->
+<html>
+    <head>
+        <title>Panel de Administrador</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="Stylesheet" href="css/bootstrap.min.css">
+        <script src="js/bootstrap.min.js"></script>
+        <link rel="Stylesheet" href="css/panelstyle.css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <!--==========Area del Menú==========-->
+                <div class="col-sm-2">
+                    <h1>Dostow</h1>
+                    <ul id="menupanel" class="nav nav-pills nav-stacked">
+                        <li class="active"><a href="panelcontrol.php"><span class="glyphicon glyphicon-th-large"></span>Panel de Control</a></li>
+                        <li class=""><a href="#"><span class="glyphicon glyphicon-plus"></span>Nuevo Post</a></li>
+                        <li class=""><a href="#"><span class="glyphicon glyphicon-tags"></span>Categorías</a></li>
+                        <li class=""><a href="#"><span class="glyphicon glyphicon-user"></span>Manage Admins</a></li>
+                        <li class=""><a href="#"><span class="glyphicon glyphicon-comment"></span>Comentarios</a></li>
+                        <li class=""><a href="#"><span class="glyphicon glyphicon-list-alt"></span>Blog</a></li>
+                        <li class=""><a href="#"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+                    </ul>
+
+                </div>
+                <!--==========Fin de área del Menú==========-->
+                <!--==========Area Principal==========-->
+                <div class="col-sm-10">
+                    <h1>Panel de Control</h1>
+                    <h4>About</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus ultrices nisi sed sem feugiat ultrices. Ut 
+                        maximus enim eget sodales hendrerit. Nunc at metus mattis,
+                        mattis urna eget, rutrum leo. Fusce augue elit, consectetur
+                        nec ullamcorper et, ultrices ac nisl. Pellentesque habitant 
+                        morbi tristique senectus et netus et malesuada fames ac 
+                        turpis egestas. In sollicitudin purus nibh, eu scelerisque
+                        diam interdum eget. Morbi nec accumsan augue. Ut pellentesque
+                        porttitor quam eu lobortis. Sed eu mollis urna. Vestibulum 
+                        scelerisque tortor non arcu sagittis venenatis. Quisque 
+                        consectetur mi eget fringilla elementum. Fusce sit amet 
+                        condimentum nibh, quis auctor diam. 
+                    </p>
+                    <h4>About</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus ultrices nisi sed sem feugiat ultrices. Ut 
+                        maximus enim eget sodales hendrerit. Nunc at metus mattis,
+                        mattis urna eget, rutrum leo. Fusce augue elit, consectetur
+                        nec ullamcorper et, ultrices ac nisl. Pellentesque habitant 
+                        morbi tristique senectus et netus et malesuada fames ac 
+                        turpis egestas. In sollicitudin purus nibh, eu scelerisque
+                        diam interdum eget. Morbi nec accumsan augue. Ut pellentesque
+                        porttitor quam eu lobortis. Sed eu mollis urna. Vestibulum 
+                        scelerisque tortor non arcu sagittis venenatis. Quisque 
+                        consectetur mi eget fringilla elementum. Fusce sit amet 
+                        condimentum nibh, quis auctor diam. 
+                    </p>
+                </div>
+                <!--==========Fin del área principal==========-->
+
+            </div>
+            <!--==========Fin del Row==========-->
+        </div>
+        <!--==========Fin del contenedor==========-->
+        <div id="footer">
+            <hr>
+            <p>Tema diseñado por <a href="http://takinformatica.weebly.com">Dostow Ugel</a> | &copy;2018 ---Todos los derechos reservados</p>
+        </div>
+        <div id="finalspace">
+
+        </div>
+    </body>
+</html>
