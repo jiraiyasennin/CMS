07b3e5cdf94200ead33eb2da8b6f6bc8dd6afed6
jiraiyasennin <d.ugel@yahoo.com>
2018-07-13 18:48:41 +0100

    Octavo día. Finalización del controller de artículos y verificación de la subida de imágenes.

--- admin/agregararticulo.php
+++ admin/agregararticulo.php
@@ -1,4 +1,10 @@
-<?php require_once '../classes/loader.php'; ?>
+<?php
+require_once '../classes/loader.php';
+/**
+ * Comienzo de la sesión
+ */
+$session = new Sesiones();
+?>
 <!DOCTYPE html>
 <!--**Página de administración de Artículos**
                By Dostow Ugel-->
@@ -9,7 +15,7 @@
             <div class="row">
                 <!--==========Area del Menú==========-->
                 <div class="col-sm-2">
-                    <h1>Dostow</h1>
+                    <h1>Menu</h1>
                     <ul id="menupanel" class="nav nav-pills nav-stacked">
                         <li class=""><a href="panelcontrol.php"><span class="glyphicon glyphicon-th-large"></span>Panel de Control</a></li>
                         <li class="active"><a href="agregararticulo.php"><span class="glyphicon glyphicon-plus"></span>Nuevo Post</a></li>
@@ -23,19 +29,15 @@
                 <!--==========Fin de área del Menú==========-->
                 <!--==========Area Principal==========-->
                 <div class="col-sm-10">
-                    <h1>Administrar Artículos</h1>
+                    <h1 id='h1Articulos'>Administrar Artículos</h1>
                     <div>
-                        <form action="agregararticulo.php" method="POST" enctype="multipart/form-data">
+                        <form class="form" action="agregararticulo.php" method="POST" enctype="multipart/form-data">
                             <fieldset>
-                                <div class="form-group">
+                                <div class="form-group col-sm-4">
                                     <label for="titulo">Título:</label>
                                     <input type="text" class="form-control" id="tituloarticulo" name="titulo" placeholder="titulo">
                                 </div>
-                                <div class="form-group">
-                                    <label for="autor">Autor:</label>
-                                    <input type="text" class="form-control" id="autor" name="autor" placeholder="autor">
-                                </div>
-                                <div class="form-group">
+                                <div class="form-group col-sm-2">
                                     <label for="categoria">Categoría:</label>
                                     <select type="text" class="form-control" id="categoria" name="categoria">
                                         <?php
@@ -46,75 +48,76 @@
                                         $listadoCategorias = $categorias->ListaDeCategorias();
                                         foreach ($listadoCategorias as $key => $data) {
                                             ?>
-                                            <option><?php echo $data['nombre']; ?></option>
+                                            <option value='<?php echo $data['id'] ?>'><?php echo $data['nombre']; ?></option>
                                         <?php } ?>
                                     </select>
                                 </div>
-                                <div class="form-group">
+                                <div class="input-group col-sm-3">
+                                    <div class="input-group input-file">
+                                        <label for="imagen">Agregar Imagen:</label>
+                                        <input type="file" class="form-control-file btn btn" id="imagenarticulo" name="imagen">
+                                    </div>
+                                </div>
+                                <div class="form-group col-sm-9">
                                     <label for="contenido">Contenido:</label>
                                     <textarea type="text" class="form-control" id="contenidoarticulo" name="contenido" placeholder="contenido"></textarea>
                                 </div>
-                                <div class="form-group">
-                                    <label for="imagen">Agregar Imagen:</label>
-                                    <input type="file" class="form-control" id="imagenarticulo" name="imagen">
+                                <div class="input-group col-sm-10 center-block">
+                                    <input  id="botonArticulo" class="btn btn-primary col-sm-4 col-sm-offset-3" type="submit" name="submit" value="Añadir Artículo">
                                 </div>
-                                <input class="btn btn-primary" type="submit" name="submit" value="Añadir Artículo">
                             </fieldset>
                         </form>
                     </div><br>
                     <div id="mensajes">
+
                         <?php
                         /**
                          * Incluir los mensajes de error
                          */
                         if (isset($_POST['submit'])) {
+
                             /**
-                             * Comienzo de la sesión
+                             * Verificación de las variables que vienen del formulario
                              */
-                            $session = new Sesiones();
-                            /**
-                             * Verificación de la variable categoria que viene del formulario
-                             */
-                            if ($_POST['categoria'] == null) {
-                                $_SESSION['error'] = "Debe escribir el nombre de la categoría";
+                            $verifica = new ArticulosControl();
+                            $verificarMasFiltrado = $verifica->VerificarNullFiltro($_POST);
+                            if (isset($verificarMasFiltrado['error'])) {
+                                $_SESSION['error'] = $verificarMasFiltrado['error'];
                                 echo $session->Error();
                             } else {
-                                /**
-                                 * Filtrar y eliminar cualquier caracter diferente de letras
-                                 *  mayúsculas o minúsculas.
-                                 */
-                                $filter = preg_replace("/[^a-zA-Z]/", "", $_POST['categoria']);
-                                /**
-                                 * Transformar el texto del input a minúsculas
-                                 */
                                 //Por ahora usaré este creador hasta que desarrolle la clase "Usuarios"
-                                $creadorCat = "dostow";
-                                $nombreCat = strtolower($filter);
-                                $categoriasObjeto = new CategoriasControl();
-                                /**
-                                 * Verificar si la categoría existe
-                                 */
-                                $verificarCategoria = $categoriasObjeto->VerificaCategoria($nombreCat);
+                                $creadorArt = "dostow";
 
-                                if ($verificarCategoria !== false) {
-                                    $_SESSION['error'] = "La categoría ya existe";
+                                /**
+                                 * Subir la imagen si está agregada
+                                 */
+                                if ($_FILES !== NULL) {
+                                    $verificaImagen = $verifica->VerificarInsertarImagen($_FILES);
+
+                                    if ($verificaImagen['error'] == true) {
+                                        $_SESSION['error'] = $verificaImagen['mensaje'];
+                                        echo $session->Error();
+                                    } else {
+                                        $_SESSION['ok'] = $verificaImagen['mensaje'];
+                                        echo $session->Exito();
+                                    }
+                                }
+                                /**
+                                 * Procedemos a insertar los datos del formulario en la base de datos
+                                 */
+                                $queryInsertArticulo = $verifica->InsertarArticulo($_POST, $creadorArt, $verificaImagen['nombreImagen']);
+
+                                if ($queryInsertArticulo == false) {
+                                    $_SESSION['error'] = "Ha habido un fallo al insertar el artículo";
                                     echo $session->Error();
                                 } else {
-                                    $categoriasObjeto->InsertaCategoria($nombreCat, $creadorCat);
-                                    if ($categoriasObjeto) {
-                                        $_SESSION['ok'] = "La categoría $nombreCat ha sido agregada";
-                                        echo $session->Exito();
-                                    } else {
-                                        $_SESSION['error'] = "Ha habido un error al agregar la categoría";
-                                        echo $session->Error();
-                                    }
+                                    $_SESSION['ok'] = "El artículo ha sido insertado correctamente";
+                                    echo $session->Exito();
                                 }
                             }
                         }
                         ?>
                     </div>
-
-
                 </div>
                 <!--==========Fin del área principal==========-->
             </div>
